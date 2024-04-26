4573de9e007d34c7b5f96fcd752018fdab77d9c6
Dedios <dediosprogramming@gmail.com>
2024-04-26 18:08:59 -0400

    refactored student scores with arrays on Program.cs

--- Program.cs
+++ Program.cs
@@ -2,29 +2,11 @@
 
 int currentAssignments = 5;
 
-int sophiaAssignment1 = 90;
-int sophiaAssignment2 = 86;
-int sophiaAssignment3 = 87;
-int sophiaAssignment4 = 98;
-int sophiaAssignment5 = 100;
+int[] sophiaAssignmentScores = new int[] { 90, 86, 90, 87, 98, 100 };
+int[] anderwiseAssignmentScores = new int[] { 92, 89, 81, 96, 90};
+int[] emmaAssignmentScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganAssignmentScores = new int[] { 90, 95, 87, 88, 96 };
 
-int andrewAssignment1 = 92;
-int andrewAssignment2 = 89;
-int andrewAssignment3 = 81;
-int andrewAssignment4 = 96;
-int andrewAssignment5 = 90;
-
-int emmaAssignment1 = 90;
-int emmaAssignment2 = 85;
-int emmaAssignment3 = 87;
-int emmaAssignment4 = 98;
-int emmaAssignment5 = 68;
-
-int loganAssignment1 = 90;
-int loganAssignment2 = 95;
-int loganAssignment3 = 87;
-int loganAssignment4 = 88;
-int loganAssignment5 = 96;
 
 int sophiaAssignmentSum = 0;
 int andrewAssignmentSum = 0;
@@ -36,11 +18,22 @@ decimal andrewScore;
 decimal emmaScore;
 decimal loganScore;
 
-sophiaAssignmentSum = sophiaAssignment1 + sophiaAssignment2 + sophiaAssignment3 + sophiaAssignment4 + sophiaAssignment5;
-andrewAssignmentSum = andrewAssignment1 + andrewAssignment2 + andrewAssignment3 + andrewAssignment4 + andrewAssignment5;
-emmaAssignmentSum = emmaAssignment1 + emmaAssignment2 + emmaAssignment3 + emmaAssignment4 + emmaAssignment5;
-loganAssignmentSum = loganAssignment1 + loganAssignment2 + loganAssignment3 + loganAssignment4 + loganAssignment5;
 
+foreach (int score in sophiaAssignmentScores) {
+    sophiaAssignmentSum += score;
+}
+
+foreach (int score in anderwiseAssignmentScores) {
+    andrewAssignmentSum += score;
+}
+
+foreach (int score in emmaAssignmentScores) {
+    emmaAssignmentSum += score;
+}
+
+foreach (int score in loganAssignmentScores) {
+    loganAssignmentSum += score;
+}
 
 sophiaScore = (decimal)sophiaAssignmentSum / currentAssignments;
 andrewScore = (decimal)andrewAssignmentSum / currentAssignments;
