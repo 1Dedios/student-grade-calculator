7d927c7ebe090b04447ce5e9b5701009f5dd6be4
Dedios <dediosprogramming@gmail.com>
2024-09-06 23:39:02 -0400

    implementing scalability of code

--- Program.cs
+++ Program.cs
@@ -1,137 +1,100 @@
 ﻿using System;
 
 int currentAssignments = 5;
+int extraAssignmentWeight = 10;
 
 int[] sophiaAssignmentScores = [90, 86, 87, 98, 100, 94, 90];
 int[] andrewAssignmentScores = [92, 89, 81, 96, 90, 89];
 int[] emmaAssignmentScores = [90, 85, 87, 98, 68, 89, 89, 89];
 int[] loganAssignmentScores = [90, 95, 87, 88, 96, 96];
 
-int sophiaAssignmentSum = 0;
-int andrewAssignmentSum = 0;
-int emmaAssignmentSum = 0;
-int loganAssignmentSum = 0;
+// put names of students in array
+string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
+int[] studentAssignmentSums = new int[currentAssignments*2];
+string[] studentLetterGrades = new string[studentNames.Length*2];
 
-decimal sophiaTotalScore;
-decimal andrewTotalScore;
-decimal emmaTotalScore;
-decimal loganTotalScore;
 
-int extraAssignmentWeight = 10;
-int gradedAssignments = 0;
 
-foreach (int score in sophiaAssignmentScores) 
+foreach (string name in studentNames)
 {
+    if (name.Equals("Sophia"))
+        studentAssignmentSums = sophiaAssignmentScores;
+    else if (name.Equals("Andrew"))
+        studentAssignmentSums = andrewAssignmentScores;
+    else if (name.Equals("Emma"))
+        studentAssignmentSums = emmaAssignmentScores;
+    else if (name.Equals("Logan"))
+        studentAssignmentSums = loganAssignmentScores;
 
-    if (gradedAssignments >= currentAssignments)
+    int gradedAssignments = 0;
+    int currentStudentSums = 0;
+    decimal currentTotalScore = 0;
+
+    foreach (int score in studentAssignmentSums) 
     {
-        sophiaAssignmentSum += score / extraAssignmentWeight;
+        if (gradedAssignments >= currentAssignments)
+        {
+            currentStudentSums += score / extraAssignmentWeight;
+        }
+        else
+        {
+            currentStudentSums += score;
+        }
+        gradedAssignments++;
     }
-    else
+
+    currentTotalScore = (decimal)currentStudentSums / currentAssignments;
+
+    // calculate the letter grade based on the total score and assign to current studentLetter
+    for (int i = 0; i < studentScores.Length; i++) 
     {
-        sophiaAssignmentSum += score;
+        decimal score = studentScores[i];
+
+        if (score >= 97) {
+            studentLetterGrades[i] = "A+";
+            
+        } else if (score >= 93) {
+            studentLetterGrades[i] = "A";
+
+        } else if (score >= 90) {
+            studentLetterGrades[i] = "A-";
+
+        } else if (score >= 87) {
+            studentLetterGrades[i] = "B+";
+
+        } else if (score >= 83) {
+            studentLetterGrades[i] = "B";
+
+        } else if (score >= 80) {
+            studentLetterGrades[i] = "B-";  
+            
+        } else if (score >= 77) {
+            studentLetterGrades[i] = "C+";
+
+        } else if (score >= 73) {
+            studentLetterGrades[i] = "C";
+
+        } else if (score >= 70) {
+            studentLetterGrades[i] = "C-"; 
+
+        } else if (score >= 67) {
+            studentLetterGrades[i] = "D+";
+
+        } else if (score >= 63) {
+            studentLetterGrades[i] = "D";
+
+        } else if (score >= 60) {
+            studentLetterGrades[i] = "D+";
+
+        } else
+            studentLetterGrades[i] = "F";
     }
-    gradedAssignments++;
+    
+
+
     
 }
 
-gradedAssignments = 0;
-
-foreach (int score in andrewAssignmentScores) 
-{
-
-    if (gradedAssignments >= currentAssignments)
-    {
-        andrewAssignmentSum += score / extraAssignmentWeight;
-    }
-    else 
-    {
-        andrewAssignmentSum += score;
-    }
-    gradedAssignments++;
-}
-
-gradedAssignments = 0;
-
-foreach (int score in emmaAssignmentScores) 
-{
-    if (gradedAssignments >= currentAssignments)
-    {
-        emmaAssignmentSum += score / extraAssignmentWeight;
-    }
-    else
-    {
-        emmaAssignmentSum += score;
-    }
-    gradedAssignments++;
-}
-
-gradedAssignments = 0;
-
-foreach (int score in loganAssignmentScores) 
-{
-    if (gradedAssignments >= currentAssignments)
-    {
-        loganAssignmentSum += score / extraAssignmentWeight;
-    }
-    else 
-    {
-        loganAssignmentSum += score;
-    }
-    gradedAssignments++;
-}
-
-sophiaTotalScore = (decimal)sophiaAssignmentSum / currentAssignments;
-andrewTotalScore = (decimal)andrewAssignmentSum / currentAssignments;
-emmaTotalScore = (decimal)emmaAssignmentSum / currentAssignments;
-loganTotalScore = (decimal)loganAssignmentSum / currentAssignments;
-
-decimal[] studentScores = {sophiaTotalScore, andrewTotalScore, emmaTotalScore, loganTotalScore};
-string[] studentLetterGrades = new string[studentScores.Length*2];
-
-for (int i = 0; i < studentScores.Length; i++) 
-{
-    decimal score = studentScores[i];
-
-    if (score >= 97) {
-        studentLetterGrades[i] = "A+";
-        
-    } else if (score >= 93) {
-        studentLetterGrades[i] = "A";
-
-    } else if (score >= 90) {
-        studentLetterGrades[i] = "A-";
-
-    } else if (score >= 87) {
-        studentLetterGrades[i] = "B+";
-
-    } else if (score >= 83) {
-        studentLetterGrades[i] = "B";
-
-    } else if (score >= 80) {
-        studentLetterGrades[i] = "B-";  
-        
-    } else if (score >= 77) {
-        studentLetterGrades[i] = "C+";
-
-    } else if (score >= 73) {
-        studentLetterGrades[i] = "C";
-
-    } else if (score >= 70) {
-        studentLetterGrades[i] = "C-"; 
-
-    } else if (score >= 67) {
-        studentLetterGrades[i] = "D+";
-
-    } else if (score >= 63) {
-        studentLetterGrades[i] = "D";
-
-    } else if (score >= 60) {
-        studentLetterGrades[i] = "D+";
-
-    } else
-        studentLetterGrades[i] = "F";
-}
 
 string sophiaLetterGrade = studentLetterGrades[0]; 
 string andrewLetterGrade = studentLetterGrades[1];
